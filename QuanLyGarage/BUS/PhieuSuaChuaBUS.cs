6e4e3b698399a12ad946573e26c391183a64ec31
lovejerryk52 <hoductruong2303@gmail.com>
2023-06-16 19:28:31 +0700

    Truong_PhieuSuaChua_chuaUpdatabase

---
+++ QuanLyGarage/BUS/PhieuSuaChuaBUS.cs
@@ -0,0 +1,49 @@
+﻿using DAO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class PhieuSuaChuaBUS
+    {
+        private static PhieuSuaChuaBUS instance;
+        public static PhieuSuaChuaBUS Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new PhieuSuaChuaBUS();
+                return instance;
+            }
+            private set
+            {
+                instance = value;
+            }
+        }
+        private PhieuSuaChuaBUS() { }
+        public DataTable LayDonGiaAndSoLuongVatTu(string ten)
+        {
+            return PhieuSuaChuaDAO.Instance.LayDonGiaAndSoLuongVatTu(ten);
+            // dt.Rows[0][0] la don gia
+            // dt.Rows[0][1] la sl hien tai trong kho
+        }
+        public int LayTienCong(string ten)
+        {
+            DataTable dt = PhieuSuaChuaDAO.Instance.LayTienCong(ten);
+            return int.Parse(dt.Rows[0][0].ToString());
+        }
+        public int ThemPhieuSuaChua(string BienSo, DateTime NgaySua, int TongTien)
+        {
+            return PhieuSuaChuaDAO.Instance.ThemPhieuSuaChua(BienSo, NgaySua, TongTien);
+        }
+        public int ThemCT_ThemPhieuSuaChua(string TenVatTu, string TenTienCong, int SoLuong)
+        {
+            return PhieuSuaChuaDAO.Instance.ThemCT_ThemPhieuSuaChua(TenVatTu, TenTienCong, SoLuong);
+        }
+
+    }
+}
---
+++ QuanLyGarage/DAO/PhieuSuaChuaDAO.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class PhieuSuaChuaDAO
+    {
+        private static PhieuSuaChuaDAO instance;
+        public static PhieuSuaChuaDAO Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new PhieuSuaChuaDAO();
+                return instance;
+            }
+            private set
+            {
+                instance = value;
+            }
+        }
+        private PhieuSuaChuaDAO() { }
+        public DataTable LayDonGiaAndSoLuongVatTu(string ten)
+        {
+            string query = "LayDonGiaAndSoLuongVatTu @TenVatTu";
+            return DataProvider.Instance.ExecuteQuery(query, new object[] { ten });
+        }
+        public DataTable LayTienCong(string ten)
+        {
+            string query = "LayTienCong @TenTienCong";
+            return DataProvider.Instance.ExecuteQuery(query, new object[] { ten });
+        }
+        public int ThemPhieuSuaChua(string BienSo, DateTime NgaySua, int TongTien)
+        {
+            string query = "ThemPhieuSuaChua @Bienso , @NgaySuaChua , @TongTien";
+            return DataProvider.Instance.ExecuteNonQuery(query, new object[] { BienSo, NgaySua, TongTien });
+        }
+        public int ThemCT_ThemPhieuSuaChua(string TenVatTu, string TenTienCong, int SoLuong)
+        {
+            string query = "ThemCT_ThemPhieuSuaChua @TenVatTu , @TenTienCong , @SoLuong";
+            return DataProvider.Instance.ExecuteNonQuery(query, new object[] { TenVatTu, TenTienCong, SoLuong });
+        }
+    }
+}
